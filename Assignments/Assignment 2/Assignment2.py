31c4210c9c04dc6ce6af581313a24f7621c52811
KRivera1 <kevin_rivera1@yahoo.com>
2020-10-04 17:07:15 -0400

    Assignment 2

--- Assignments/Assignment 2/Assignment2.py
+++ Assignments/Assignment 2/Assignment2.py
@@ -18,25 +18,25 @@
 
 #---------------------------------------Libraries------------------------------------------
 
-from random import randint			#Library that enables the ability to generate random numbers.
+from random import randint		#Library that enables the ability to generate random numbers.
 from sense_hat import SenseHat		#Library that enables the sense hat functionality.
-import time							#Library that enable the ability to use time tools.
+import time				#Library that enable the ability to use time tools.
 
 #---------------------------------------Variables------------------------------------------
 
-sense = SenseHat()					#Object created from the sense hat library.
-sense.clear()						#Function call to clear the sense hat matrix LED.
-counter = 0							#Variable that will keep track of the number of loop cycles.
+sense = SenseHat()			#Object created from the sense hat library.
+sense.clear()				#Function call to clear the sense hat matrix LED.
+counter = 0				#Variable that will keep track of the number of loop cycles.
 
 #---------------------------------------Loop-----------------------------------------------
 
 while True:
 	
-	orient = sense.get_orientation()		#Tuple that contains the mems gyroscope data.
+	orient = sense.get_orientation()	#Tuple that contains the mems gyroscope data.
 	accel = sense.get_accelerometer_raw()	#Tuple that contains the mems accelerometer data.
-	pitch = orient["pitch"]					#Assigning the pitch data to a variable.
-	yaccel = abs(accel['y'])				#Assigning the absolute value Y acceleration data to a variable.
-	zaccel = abs(accel['z'])				#Assigning the absolute value Z acceleration data to a variable.
+	pitch = orient["pitch"]			#Assigning the pitch data to a variable.
+	yaccel = abs(accel['y'])		#Assigning the absolute value Y acceleration data to a variable.
+	zaccel = abs(accel['z'])		#Assigning the absolute value Z acceleration data to a variable.
 	
 	#If statement checks to make sure that reassigns accelerator values if they are greater than 1.
 	if yaccel > 1:
@@ -46,18 +46,18 @@ while True:
 		zaccel = 1
 	
 	
-	xcoord = randint(0, 7)					#Random value between 0-7 assigned to the xcoord value.
-	ycoord = randint(0, 7)					#Random value between 0-7 assigned to the ycoord value.
-	r = int(255*yaccel)						#'Red' part of LED calculated. Value between 0-255.
-	b = int(255*zaccel)						#'Blue' part of LED calculated. Value between 0-255.
+	xcoord = randint(0, 7)			#Random value between 0-7 assigned to the xcoord value.
+	ycoord = randint(0, 7)			#Random value between 0-7 assigned to the ycoord value.
+	r = int(255*yaccel)			#'Red' part of LED calculated. Value between 0-255.
+	b = int(255*zaccel)			#'Blue' part of LED calculated. Value between 0-255.
 	sense.set_pixel(xcoord, ycoord, r, 0, b)#The next LED pixel is enabled and given an xy coordinate and color between blue and red.
-	counter = counter + 1					#The counter is incremented by one.
+	counter = counter + 1			#The counter is incremented by one.
 	
 	#If statement that checks the value of the counter variable. If the counter value is 30 then the
 	#LED matrix is cleared to prevent cluttering. The counter variable is reset as well.
 	if counter == 30:
-		sense.clear()						#Function call to clear the sense hat matrix LED.
+		sense.clear()			#Function call to clear the sense hat matrix LED.
 		counter = 0
 		
 		
-	time.sleep(pitch/1000)					#The raspberry pi is paused for a fraction of a second before lighting another LED.
+	time.sleep(pitch/1000)			#The raspberry pi is paused for a fraction of a second before lighting another LED.
