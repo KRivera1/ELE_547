524fccbae4e90fdf1e6c20deef2bf75bf7f2d771
KRivera1 <kevin_rivera1@yahoo.com>
2020-10-04 14:49:37 -0400

    Assignment 2

--- Assignments/Assignment 2/Assignment2.py
+++ Assignments/Assignment 2/Assignment2.py
@@ -6,24 +6,36 @@ sense = SenseHat()
 sense.clear()
 a = 1
 counter = 0
-#sense.set_pixel(0, 0, 255, 0, 0) 
-#sense.set_pixel(0, 7, 0, 255, 0) 
-#sense.set_pixel(7, 0, 0, 0, 255)
-#sense.set_pixel(7, 7, 255, 0, 255)
-#time.sleep(2)
-#sense.clear()
+
 while a:
-	#print("Hello")
-	#time.sleep(1)
-	x = randint(0, 7)
-	y = randint(0, 7)
-	r = randint(0, 100)
-	g = randint(0, 100)
-	b = randint(0, 100)
-	sense.set_pixel(x, y, r, g, b)
+	
+	orient = sense.get_orientation()
+	accel = sense.get_accelerometer_raw()
+	pitch = orient["pitch"]
+	roll = orient["roll"]
+	yaw = orient["yaw"]
+	xaccel = accel['x']
+	yaccel = abs(accel['y'])
+	zaccel = abs(accel['z'])
+	if yaccel > 1:
+		yaccel = 1
+	if zaccel > 1:
+		zaccel = 1
+	xcoord = randint(0, 7)
+	ycoord = randint(0, 7)
+	#r = randint(0, 100)
+	#g = randint(0, 100)
+	#b = randint(0, 100)
+	r = int(255*yaccel)
+	b = int(255*zaccel)
+	#sense.set_pixel(xcoord, ycoord, r, g, b)
+	sense.set_pixel(xcoord, ycoord, r, 0, b)
 	counter = counter + 1
-	if counter == 15:
-		a = 0
-	time.sleep(1)
+	if counter == 30:
+		#a = 0
+		sense.clear()
+		counter = 0
+	time.sleep(pitch/1000)
+	print(pitch)
 
 sense.clear()
