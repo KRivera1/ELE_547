121de143e7ced5232f393bc2180321807cbdfa29
Bairdx2 <pokeman1993@yahoo.com>
2020-10-10 19:32:30 -0400

    Assignment 3

--- Assignments/Assignment 3/Assignment3.py
+++ Assignments/Assignment 3/Assignment3.py
@@ -10,11 +10,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def senseData():
-	temp = sense.get_temperature()
-	humidity = sense.get_humidity()
-	tempStr = str(temp)
-	humStr = str(humudity)
-	return tempStr
+	temp = str(sense.get_temperature())
+	humidity = str(sense.get_humidity())
+	#tempStr = str(temp)
+	#humStr = str(humudity)
+	Data = {'temp':temp, 'humidity':humidity}
+	return render_template('senseData.html',**Data)
 	
 if __name__ == '__main__':
 	app.run(host = '192.168.0.13', debug = True)
