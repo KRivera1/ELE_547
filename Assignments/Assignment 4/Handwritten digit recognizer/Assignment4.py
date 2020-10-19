f67481d723454e5f51f8e740a41d190fe4b1f9e4
Bairdx2 <pokeman1993@yahoo.com>
2020-10-19 00:57:21 -0400

    Assignment 4

--- Assignments/Assignment 4/Handwritten digit recognizer/Assignment4.py
+++ Assignments/Assignment 4/Handwritten digit recognizer/Assignment4.py
@@ -1,23 +1,24 @@
 # Script: Assignment4.py
-# Description: This is a program to practice creating a webserver
-#			   using the Raspberry Pi. The webserver contains a table
-#			   that displays the temperature and humidity sensor data
-#			   being collected from the sensehat. The webserver updates
-#			   every 5 seconds to display the latest data.
+# Description: This is a program to practice utilizing artificial
+#			   neural networks. Hand written numerical digits between
+#			   0 - 9 can be recognized by utilizing a model derived
+#			   from a training list program. For best results, draw the
+#			   number at the center of the drawing canvas and make it large
+#			   enough to cover the majority of the drawing canvas.
 #			   
-# Author: Kevin Rivera
+# Author: Kevin Rivera, DataFlair website
 # Version: 1.0
 # Date: 10-16-2020
 # ELE 547 Assignment 3
 
-#from keras.models import load_model
+from keras.models import load_model
 from tkinter import *
 import tkinter as tk
 from PIL import Image
 #import pyscreenshot as ImageGrab
 import numpy as np
 
-#model = load_model('mnist.h5')
+model = load_model('mnist.h5')
 #model = load_model('mnist.sig')
 
 
@@ -52,6 +53,7 @@ class App(tk.Tk):
     def clear_all(self):
         self.canvas.delete("all")
     def classify_handwriting(self):
+		#Utilizing the tkinter built in tools to get the drawing canvas window information.
         box = (self.canvas.winfo_rootx(), self.canvas.winfo_rooty(), self.canvas.winfo_rootx() + self.canvas.winfo_width(), self.canvas.winfo_rooty() + self.canvas.winfo_height())
         im = Image.grab(bbox = box)
         digit, acc = predict_digit(im)
